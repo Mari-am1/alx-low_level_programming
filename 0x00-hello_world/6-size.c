4250da26a39fcfc006f92954258b39297239da87
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-06 01:05:47 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,9 @@
  * main - A program that print a line using the printf function
  * Return: 0 (Success)
  */
-
 int main(void)
 {
+
 int x;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
@@ -13,4 +13,5 @@ printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
+
 }
