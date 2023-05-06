9f8f0ccbf48056af36dc9a232106c506553a3a85
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-06 11:20:00 -0700

    d

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,37 +2,38 @@
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
+*
 * Return: the int converted from the string
 */
 int _atoi(char *s)
 {
-int x, y, z, m, h, f;
-x = 0;
-y = 0;
-z = 0;
-m = 0;
-h = 0;
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
 f = 0;
-while (s[m] != '\0')
-m++;
-while (x < m && h == 0)
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
-if (s[x] == '-')
-++y;
-if (s[m] >= '0' && s[x] <= '9')
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
 {
-f = s[x] - '0';
-if (y % 2)
-f = -f;
-z = z * 10 + f;
-h = 1;
-if (s[x + 1] < '0' || s[x + 1] > '9')
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
-h = 0;
+f = 0;
 }
-x++;
+i++;
 }
-if (h == 0)
+if (f == 0)
 return (0);
-return (z);
+return (n);
 }
