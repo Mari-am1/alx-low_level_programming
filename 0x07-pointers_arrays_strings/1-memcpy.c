708ccc0bcd0d6d29310949a7eba197c9a7fd19d8
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-08 06:56:10 -0700

    d

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,9 @@
 
 /**
 * *_memcpy - function copies n bytes from memory area src to memory area dest
-* @dest: char   
-* @src: char 
-* @n: unsigned int 
+* @dest:" the destination that we will copy  source into         "
+* @src:" the source that we will copy  destination  into         "
+* @n: max bytes we can use and copy it from memory area src to memory area dest
 * Return: dest
 */
 
