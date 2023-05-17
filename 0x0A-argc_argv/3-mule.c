4225fcf61bb0925e20c35010dd458bd6967fd776
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-17 14:44:04 -0700

    it is not hurt as it was

--- 0x0A-argc_argv/3-mule.c
+++ 0x0A-argc_argv/3-mule.c
@@ -4,40 +4,39 @@
 /**
 * _atoi - converts a string to an integer
 * @s: string to be converted
-*
 * Return: the int converted from the string
 */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
+int m, h, j, n, f, d;
 
-i = 0;
-d = 0;
+m = 0;
+h = 0;
+j = 0;
 n = 0;
-len = 0;
 f = 0;
-digit = 0;
+d = 0;
 
-while (s[len] != '\0')
-len++;
+while (s[n] != '\0')
+n++;
 
-while (i < len && f == 0)
+while (m < n && f == 0)
 {
-if (s[i] == '-')
-++d;
+if (s[m] == '-')
+++h;
 
-if (s[i] >= '0' && s[i] <= '9')
+if (s[m] >= '0' && s[m] <= '9')
 {
-digit = s[i] - '0';
+d = s[m] - '0';
 if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
+d = -d;
+j = j * 10 + d;
 f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
+if (s[m + 1] < '0' || s[m + 1] > '9')
 break;
 f = 0;
 }
-i++;
+m++;
 }
 
 if (f == 0)
