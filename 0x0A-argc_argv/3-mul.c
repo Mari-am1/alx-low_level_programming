4a1a0b9966a67581c27b52b650c1eea0c153cb55
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-17 13:46:12 -0700

    it is not hurt as it was

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,71 +1,72 @@
-#include <stdio.h>
-#include "main.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
+* _atoi - converts a string to an integer
+* @s: string to be converted
+* Return: the int converted from the string
+*/
+int _atoi(char *s)
 {
- int i, d, n, len, f, digit;
- i = 0;
- d = 0;
- n = 0;
- len = 0;
- f = 0;
- digit = 0;
+int m, h, j, n, f, d;
 
- while (s[len] != '\0')
-  len++;
+m = 0;
+h = 0;
+j = 0;
+n = 0;
+f = 0;
+d = 0;
 
- while (i < len && f == 0)
- {
-  if (s[i] == '-')
-   ++d;
+while (s[n] != '\0')
+n++;
 
-  if (s[i] >= '0' && s[i] <= '9')
-  {
-   digit = s[i] - '0';
-   if (d % 2)
-    digit = -digit;
-   n = n * 10 + digit;
-   f = 1;
-   if (s[i + 1] < '0' || s[i + 1] > '9')
-    break;
-   f = 0;
-  }
-  i++;
- }
+while (m < n && f == 0)
+{
+if (s[m] == '-')
+++d;
 
- if (f == 0)
-  return (0);
+if (s[m] >= '0' && s[m] <= '9')
+{
+d = s[m] - '0';
+if (d % 2)
+d = -d;
+j = j * 10 + d;
+f = 1;
+if (s[m + 1] < '0' || s[m + 1] > '9')
+break;
+f = 0;
+}
+m++;
+}
 
- return (n);
+if (f == 0)
+return (0);
+
+return (n);
 }
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
-int main(int argc, char *argv[])
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 (Success), 1 (Error)
+*/
+int main(int argc, char *argv[])
 {
- int result, num1, num2;
+int result, num1, num2;
 
- if (argc < 3 || argc > 3)
- {
-  printf("Error\n");
-  return (1);
- }
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
 
- num1 = _atoi(argv[1]);
- num2 = _atoi(argv[2]);
- result = num1 * num2;
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+result = num1 *num2;
 
- printf("%d\n", result);
+printf("%d\n", result);
 
- return (0);
+return (0);
 }
