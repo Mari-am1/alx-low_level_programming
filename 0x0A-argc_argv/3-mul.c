5e5f27409545cf8c71b04e697b740899fc3c8be5
Mari-am1 <mariammostafafh13@gmail.com>
2023-05-17 14:46:50 -0700

    it is not hurt as it was

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,29 +9,29 @@
 */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
+int i, d, n, l, f, d;
 
 i = 0;
 d = 0;
 n = 0;
-len = 0;
+l = 0;
 f = 0;
-digit = 0;
+d = 0;
 
-while (s[len] != '\0')
-len++;
+while (s[l] != '\0')
+l++;
 
-while (i < len && f == 0)
+while (i < l && f == 0)
 {
 if (s[i] == '-')
 ++d;
 
 if (s[i] >= '0' && s[i] <= '9')
 {
-digit = s[i] - '0';
+d = s[i] - '0';
 if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
+d = -;
+n = n * 10 + d;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
