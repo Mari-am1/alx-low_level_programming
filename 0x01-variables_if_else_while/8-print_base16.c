d702b0070952b4efc535223bbbe27b8ddb579edf
Mari-am1 <mariammostafafh13@gmail.com>
2023-07-26 08:19:44 -0700

    i forget my memories

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+int alp = 97;
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+while (alp != 103)
+{
+putchar(alp);
+alp++;
+}
+putchar('\n');
+}
