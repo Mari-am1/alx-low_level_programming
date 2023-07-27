927151c6608e6f61449cc59acebd7134980eebda
Mari-am1 <mariammostafafh13@gmail.com>
2023-07-27 16:30:25 -0700

    i wanna be better than yesterday

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,17 +5,14 @@
 /**
  * _islower - check if the character is lowercase or not.
  *
+ *@c:function parameter
+ *
  * Return: Always 0.
  */
 int _islower(int c)
 {
-int r;
-r = _islower('H');
-putchar(r + '0');
-r = _islower('o');
-putchar(r + '0');
-r = _islower(108);
-putchar(r + '0');
-putchar('\n');
+if (c >= 'a' && c <= 'z')
+return (1);
+else
 return (0);
 }
