a10be5dc0e1bdd35f412c2ead10bc91b497c4d3e
Mari-am1 <mariammostafafh13@gmail.com>
2023-07-30 09:11:26 -0700

    git

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,23 @@
 #include "main.h"
 /**
- * print_line - function that draws a straight line
+ * print_diagonal - function that draws a diagonal line on the terminal
  *
  * @n: function parameter
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
 if (n > 0)
 {
-int x;
+int x, z;
+int y = 1;
 for (x = 0; x < n; x++)
-_putchar('_');
+{
+for (z = 0; z < y; z++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+y++;
+}
 }
 _putchar('\n');
 }
