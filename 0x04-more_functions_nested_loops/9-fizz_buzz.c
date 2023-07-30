42ea60f9d5f928192841fb120882fdcdbe50bed1
Mari-am1 <mariammostafafh13@gmail.com>
2023-07-30 09:42:25 -0700

    git

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print numbers by trick way
+ *
+ */
+void main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (((x % 3) == 0) && ((x % 5) == 0))
+printf("FizzBuzz ");
+else if ((x % 3) == 0)
+printf("Fizz ");
+else if ((x % 5) == 0)
+printf("Buzz ");
+else
+printf("%d ", x);
+}
+printf("\n");
+}
