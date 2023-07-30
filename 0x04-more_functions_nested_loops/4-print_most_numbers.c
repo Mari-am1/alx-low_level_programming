0b498b0aacb8a33af59edc9fc11b0e5e618b4b87
Mari-am1 <mariammostafafh13@gmail.com>
2023-07-30 07:06:36 -0700

    git

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that print the most of the numbers
+ *
+ */
+void print_most_numbers(void)
+{
+int x;
+for (x = '0'; x < 10; x++)
+{
+if (x < 2 || x == 3 || x > 4)
+_putchar(x + '0');
+}
+}
