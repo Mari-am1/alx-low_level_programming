39c06f7e34af66fad0b8502ac53ad3b33ef002e9
Mari-am1 <mariammostafafh13@gmail.com>
2023-09-21 12:37:38 -0700

    0x0A-argc_argv updates

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,11 +12,11 @@
  */
 int main(int argc, char **argv)
 {
+int i = 0, sum = 0;
 if (argc == 1)
 printf("0\n");
 else
 {
-int i = 0, sum = 0;
 for (i = 1; i < argc; i++)
 {
 if (*argv[i] >= '0' && *argv[i] <= '9')
