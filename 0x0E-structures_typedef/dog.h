94789024b034ae0a37ec614534308dab1401e2df
Mari-am1 <mariammostafafh13@gmail.com>
2023-11-24 05:44:16 -0800

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure contain 3 members
+ * @name:the first member of the struct
+ * @age: the second
+ * @owner:the third
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
